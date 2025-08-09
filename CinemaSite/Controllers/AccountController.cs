964a684de0705dc43844fc131aac5b72b296a1ac
hat-hat <firecv.x@gmail.com>
2025-08-09 22:03:29 +0200

    Controller fixes

--- CinemaSite/Controllers/AccountController.cs
+++ CinemaSite/Controllers/AccountController.cs
@@ -1,22 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CinemaSite.Data;
+using CinemaSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CinemaSite.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<AccountController> _logger;
+        private readonly CinemaDbContext _context;
+
+        public AccountController(ILogger<AccountController> logger, CinemaDbContext context)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public IActionResult Rejestracja()
         {
             return View();
         }
 
-        public IActionResult NowyUzytkownik()
+        public IActionResult NowyUzytkownik(UserAccountEntity nowyUzytkownik)
         {
             if (ModelState.IsValid)
             {
-
+                _context.UserAccount.Add()
             }
 
-            return RedirectToAction("Index");
+            return RedirectToAction("Rejestracja");
         }
     }
 }
--- CinemaSite/ViewModels/AccountViewModel.cs
+++ CinemaSite/ViewModels/AccountViewModel.cs
@@ -5,5 +5,11 @@ namespace CinemaSite.ViewModels
     public class AccountViewModel
     {
         public List<UserAccountEntity> UserAccounts { get; set; }
+
+        public string username { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string password_hash { get; set; }
+
     }
 }
