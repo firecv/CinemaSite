6471b5982acb3f3953dd98076c9374c1b06b654a
hat-hat <firecv.x@gmail.com>
2025-09-10 19:00:32 +0200

    Untaken seats will recognize clicks and change colors

--- CinemaSite/wwwroot/js/seat-selection.js
+++ CinemaSite/wwwroot/js/seat-selection.js
@@ -1,1 +1,17 @@
-﻿
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const seatGrid = document.getElementById("seat-grid");
+    //const seats = Array.from(document.querySelectorAll(".seat-square"));
+
+    seatGrid.addEventListener("click", (e) => {
+        const seat = e.target.closest(".seat-square");
+
+        if (seat == null || seat.classList.contains("taken")) return;
+
+
+        seat.classList.toggle("selected");
+
+        //ADD TO CART
+
+
+    });
+});
