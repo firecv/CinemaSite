ad3cd2709822880bd84308afcca5e3a469ccb67b
Ethan Hattingh <firecv.x@gmail.com>
2025-10-04 19:21:02 +0200

    Nav menu css changes

---
+++ CinemaSite/wwwroot/js/navbar-behaviour.js
@@ -0,0 +1,8 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const openBurgerButton = document.getElementById("open-hamburger");
+    const burgerMenu = document.getElementById("dropdown-hamburger");
+
+    openBurgerButton.addEventListener("click", () => {
+        burgerMenu.classList.toggle("popout");
+    });
+});
