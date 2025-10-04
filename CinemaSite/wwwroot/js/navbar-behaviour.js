a20b150737c2eb49ce2507fcf69850ecd256d47b
Ethan Hattingh <firecv.x@gmail.com>
2025-10-04 20:07:09 +0200

    Popout header menu for mobile/small screens

--- CinemaSite/wwwroot/js/navbar-behaviour.js
+++ CinemaSite/wwwroot/js/navbar-behaviour.js
@@ -1,8 +1,12 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
-    const openBurgerButton = document.getElementById("open-hamburger");
+    const openBurgerButton = document.getElementById("hamburger-button");
+    const closeBurgerButton = document.getElementById("hamburger-button2");
     const burgerMenu = document.getElementById("dropdown-hamburger");
 
     openBurgerButton.addEventListener("click", () => {
+        burgerMenu.classList.toggle("popout");
+    });
+    closeBurgerButton.addEventListener("click", () => {
         burgerMenu.classList.toggle("popout");
     });
 });
